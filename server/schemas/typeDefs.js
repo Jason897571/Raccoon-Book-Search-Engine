f0fd4fd1e39c55d1955acb344e6053cc8e8e3ca7
Jason897571 <king.gs1314@outlook.com>
2024-04-19 14:08:42 -0400

    complete setup for server.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const typeDefs =`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String
+        author: String
+        description: String
+        title: String
+        image: String
+        link: String
+        
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(author: String!, description: String!, bookId: String!, image: String!, link: String!, title: String!): User
+        removeBook(bookId: ID!): User
+    }
+        
+    
+
+
+
+
+
+`
